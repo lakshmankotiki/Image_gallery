c4519bed094459f01fdbdb14f47e1292ca6cef90
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-01-08 00:57:36 +0530

    added all images and wrote click handler on images

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+var imgSelector = document.querySelectorAll("img");
+for(var i=1;i<imgSelector.length;i++) {
+    document.querySelectorAll("img")[i].addEventListener("click", function() {
+        window.alert("clicked man...");
+    });
+}
 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src', "");
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
