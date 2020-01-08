e01459112cefb8d6b913f9650604dc7316796e72
Lakshman Kotiki <kotiki.lakshman1996@gmail.com>
2020-01-08 16:43:24 +0530

    applied darken and lighten on click of darken button and updated displayed-img src on click of image

--- main.js
+++ main.js
@@ -4,21 +4,30 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
+var bottomImages = document.querySelectorAll(".bottom-img");
+
 /* Looping through images */
-for(var i=0;i<displayedImage.length;i++) {
-    displayedImage[i].addEventListener("click", function() {
-        window.console.log(this.className.slice(13, 18));
+for (var i = 0; i < bottomImages.length; i++) {
+    bottomImages[i].addEventListener("click", function() {
+        var srcPath = this.src;
+        // window.console.log(srcPath);
+        document.querySelector(".displayed-img").setAttribute("src", srcPath);
+        // window.console.log(updatedSrc);
     });
 }
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', "");
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
 document.querySelector(".dark").addEventListener("click", function(event) {
     var buttonText = document.querySelector(".dark").innerHTML;
-    if(buttonText === "darken") {
-
+    window.console.log(buttonText);
+    if (buttonText === "Darken") {
+        // window.console.log("inside");
+        document.querySelector(".overlay").style.cssText = "background: rgba(150,150,150,0.6)";
+        document.querySelector(".dark").innerHTML = "Lighten";
+        document.querySelector(".dark").style.cssText = "background-color: white";
+    } else if (buttonText === "Lighten") {
+        document.querySelector(".overlay").style.cssText = "opacity: 0";
+        document.querySelector(".dark").innerHTML = "Darken";
+        document.querySelector(".dark").style.cssText = "background: rgba(150,150,150,0.6)";
     }
 });
